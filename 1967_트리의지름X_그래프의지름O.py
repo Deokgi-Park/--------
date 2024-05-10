0f5e555251c92af0b11222395b2f4e40b37f2fdb
Deokgi-Park <ejrrl6931@naver.com>
2024-05-10 15:51:14 +0900

    5/10

--- 1967_트리의지름X_그래프의지름O.py
+++ 1967_트리의지름X_그래프의지름O.py
@@ -1,5 +1,5 @@
 import sys
-
+sys.setrecursionlimit(100000000)
 N = int(input())
 data = [[] for _ in range(N+1)]
 visited = [False] * (N+1)
@@ -8,22 +8,19 @@ while True:
     try:
         index, next, dis = map(int, sys.stdin.readline().strip().split())
         data[index].append([next,dis])
+        data[next].append([index,dis])
     except:
         break
 
 def dfs(root, dis):
-    if not data[root] :
-        return dis
     visited[root] = True
-    node_list = []
     for i in data[root] :
         if not visited[i[0]] :
-            val = dfs(i[0], i[1])
-            node_list.append(val)
-    maxdis.append(sum(node_list))
-    return max(node_list) + dis
-
-dfs(1, 0)     
-# print(data)
-# print(visited)
-print(max(maxdis))
+            dfs(i[0], dis + i[1])
+    maxdis.append([dis,root])
+dfs(1, 0)
+maxdis.sort(reverse=True)
+visited = [False] * (N+1)
+dfs(maxdis[0][1], 0)
+maxdis.sort(reverse=True)
+print(maxdis[0][0])
